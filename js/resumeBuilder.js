e4cf0ed2cc9f5394f1a9e5e23aa6e9e81871dbc0
Marcus Carraschi <marcuscarraschy@gmail.com>
2022-01-24 21:29:04 -0300

    Personal information - work description

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -45,9 +45,86 @@ var bio = {
 
 };
 
+var work = {
+    jobs: [
+        {
+            employer: 'Linx (Stone Group)',
+            title: 'Full Stack Software Developer - Pl',
+            location: 'Remote Work - Brazil',
+            dates: 'November 2021 - Until now',
+            description: ' I working as a full stack software development at Linx on digital' +
+                'retail solutions using .NET, RESTfull API, TypeScript/VueJs, ' +
+                'relational databases (e.g. SQL Server, Dapper) and Azure Devops.'
+        },
+        {
+            employer: 'Atech - Negócios em tecnologias S/A',
+            title: 'Junior Full Stack Software Developer',
+            location: 'São Paulo - Brazil',
+            dates: 'November 2019 - October 2021',
+            description: 'Development of internal software and systems used by the departments of ' +
+                'company (e.g. document managements, human resource process, APIs extractions) ' +
+                'using .NET Core, Python, HTML, CSS, Javascript, SQL Server, Oracle DB.<br>' +
+                'Process automation and utility scripts development utilizing Bash and Groovy ' +
+                '(e.g. application deployment, pipelines);<br>' +
+                'The deployment of applications were made by Jenkins and Docker on machines with ' +
+                'CentOs operating system;<br>' +
+                'Build management and DevOps of air traffic management systems Git, GitLab Runners ' +
+                'and SonarQube (e.g. version control).'
+        },
+        {
+            employer: 'Matrix Sistemas',
+            title: 'Junior Full Stack Developer',
+            location: 'São Paulo - Brazil',
+            dates: 'May 2019 - October 2019',
+            description: 'Full stack development on the CRM system. I worked with development software' +
+                'routines using .NET, web technologies (e.g. bootstrap framework, javascript) ' +
+                'and database Sql Server. I studied and make a Progressive Web Application to apply ' +
+                'in the system of company.'
+        },
+        {
+            employer: 'EGS - Ericsson Gestão e Serviços',
+            title: 'Software Developer Inter',
+            location: 'São Paulo - Brazil',
+            dates: 'May 2018 - January 2019',
+            description: 'I worked in project of Vivo company with ETL process (e.g. data transformation, ' +
+                'OLAP cube) using PowerCenter tool and Oracle database.'
+        },
+        {
+            employer: 'GAC Group (Brazil)',
+            title: 'Innovation Intern',
+            location: 'São Paulo - Brazil',
+            dates: 'October 2016 - April 2018',
+            description: 'Follow-up of qualify projects in law 11.196/05 and assessment of the' +
+                'eligibility of new projects for the tax benefit. Assistance in organizing' +
+                'project and product documents, monitoring process mapping, preparing' +
+                'descriptions of technology projects adopted by large corporations (e.g. insurance' +
+                'companies, investment banking, billing systems).'
+        }
+    ],
+    display: function () {
+        if (work.jobs.length > 0) {
+            for (var i = 0, len = work.jobs.length; i < len; i++) {
+                var formattedEmployer = HTMLworkEmployer.replace(DATA, work.jobs[i].employer);
+                var formattedTitle = HTMLworkTitle.replace(DATA, work.jobs[i].title);
+                var formattedDates = HTMLworkDates.replace(DATA, work.jobs[i].dates);
+                var formattedLocation = HTMLworkLocation.replace(DATA, work.jobs[i].location);
+                var formattedDescription = HTMLworkDescription.replace(DATA, work.jobs[i].description);
+                var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+                $('#workExperience').append(HTMLworkStart);
+                $('.work-entry:last').append(formattedEmployerTitle);
+                $('.work-entry:last').append(formattedDates);
+                $('.work-entry:last').append(formattedLocation);
+                $('.work-entry:last').append(formattedDescription);
+            }
+        }
+
+    }
+};
+
 
 bio.display();
-education.display();
+//education.display();
 work.display();
 projects.display();
 $('#mapDiv').append(googleMap);
